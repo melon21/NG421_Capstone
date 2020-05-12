2f6d65d95bcb293ce690fb3d100e465bf9b704d7
Melinda Marino <melindamarino22@gmail.com>
2020-05-12 18:31:01 -0500

    dotnet weather updates

---
+++ Data/Migrations/20200512232434_AddedWeather.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace capstone.Data.Migrations
+{
+    public partial class AddedWeather : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.CreateTable(
+                name: "Weather",
+                columns: table => new
+                {
+                    weatherForecast = table.Column<string>(nullable: false),
+                    temprature = table.Column<int>(nullable: false),
+                    humidity = table.Column<int>(nullable: false),
+                    timeOfDay = table.Column<string>(nullable: true),
+                    date = table.Column<string>(nullable: true),
+                    wind = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weather", x => x.weatherForecast);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weather");
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 1, "Jon", "Smith" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 2, "Bobby", "Miller" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 3, "Sarah", "Brooks" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 1, "Sam", "Smith" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 2, "Tom", "Miller" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 3, "Mary", "Brooks" });
+        }
+    }
+}

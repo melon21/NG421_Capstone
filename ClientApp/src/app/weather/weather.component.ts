9f7e683cb1cfeb911d02be5ebabbc5fa527c6dc8
Melinda Marino <melindamarino22@gmail.com>
2020-05-13 04:18:07 -0500

    weather ts

--- ClientApp/src/app/weather/weather.component.ts
+++ ClientApp/src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-weather',
@@ -6,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  public weather: Weather[];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+       
+    }
+    async ngOnInit() {
+        this.weather = await this.http.get<Weather[]>(this.baseUrl + 'weather').toPromise();
+    }
 
 }
 interface Weather {
-
+weatherForcast: string;
+temprature: number;
+humidity: number;
+timeOfDay: string;
+date: number;
+wind: number;
 }

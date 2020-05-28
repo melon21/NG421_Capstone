a26f9e0303afa4aded477a7cda8fdeb9fb31581f
Melinda Marino <melindamarino22@gmail.com>
2020-05-27 22:56:45 -0500

    Forecast service

---
+++ ClientApp/src/app/services/forecast.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForecastService {
+
+  API_URL = 'https://dataservice.accuweather.com/forecasts/v1/daily/1day/331131?apikey=RF9jPHpFC8IXHNAZUGrdC9BPQO93mzI8&language=en-us&details=false&metric=false';
+  API_KEY = 'RF9jPHpFC8IXHNAZUGrdC9BPQO93mzI8';
+  constructor(private httpService: HttpClient) {}
+
+  get(searchTerm: string): Promise<any> {
+
+    const params: HttpParams = new HttpParams()
+    .set('api_key', this.API_KEY)
+    .set('query', searchTerm);
+
+    return this.httpService.get<any>(this.API_URL, {params}).toPromise();
+  }
+}

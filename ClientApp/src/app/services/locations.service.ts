daf9299763d547e7670870593e2513421c1d1e9e
Melinda Marino <melindamarino22@gmail.com>
2020-08-20 18:49:42 -0500

    font fixed and locations service made

--- ClientApp/src/app/services/locations.service.ts
+++ ClientApp/src/app/services/locations.service.ts
@@ -1,20 +1,22 @@
+
+
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Weather } from 'src/app/interfaces/weather';
+import { Locations } from 'src/app/interfaces/locations';
 import { LocationApiService } from './location-api.service';
 import { ForecastApiService } from './forecast-api.service';
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherService {
+export class LocationsService {
 
   // tslint:disable-next-line: max-line-length
   constructor (private httpClient: HttpClient, @Inject('BASE URL')private baseURL: string,  private locationApiService: LocationApiService, private forecastApiService: ForecastApiService, ) { }
 
- async getWeather() {
-  return this.httpClient.get<Weather[]>('${this.baseURL}weather').toPromise();
+ async getLocations() {
+  return this.httpClient.get<Locations[]>('${this.baseURL}locations').toPromise();
  }
- async addWeather() {
-  return await this.httpClient.post<Weather[]>('${this.baseURL}weather', WeatherService).toPromise();
+ async addLocaations() {
+  return await this.httpClient.post<Locations[]>('${this.baseURL}locations', LocationsService).toPromise();
 }
 }

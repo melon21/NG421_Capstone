33142eaba00939dd8299cc9348d49c54032ca2e4
Melinda Marino <melindamarino22@gmail.com>
2020-08-24 03:46:23 -0500

    save

--- ClientApp/src/app/locations/locations.component.ts
+++ ClientApp/src/app/locations/locations.component.ts
@@ -12,8 +12,8 @@ import { MatTableDataSource } from '@angular/material/table';
 export class LocationsComponent implements OnInit {
 
 public locations: Locations[];
-  public newLocations: Locations[] = [{locationName: '', country: '',  adminArea: '', key: 0}];
-  displayedColumns: string[] = ['locationName', 'country', 'adminArea', 'key'];
+  public newLocations: Locations[] = [{locationName: '', country: '', adminArea: '', key: 0}];
+  displayedColumns: string [] = ['locationName', 'country', 'adminArea', 'key'];
 dataSource: MatTableDataSource<Locations>;
 
 
@@ -25,37 +25,28 @@ async ngOnInit() {
   this.locations = await this.locationApiService.get('');
   console.log(this.locations);
 
-  this.newLocations[0].locationName = this.locations['EnglishName'][0];
+    this.newLocations[0].locationName = this.locations['EnglishName'];
     console.log(this.newLocations);
 
-    this.newLocations[0].country = this.locations['country'][0]['EnglishName'];
+    this.newLocations[0].country = this.locations['Country'];
     console.log(this.newLocations);
 
-    this.newLocations[0].adminArea = this.locations['AdminastraitiveArea'][0]['EnglishName'];
+    this.newLocations[0].adminArea = this.locations['AdministrativeArea'];
     console.log(this.newLocations);
 
-     this.newLocations[0].key = this.locations[0]['key'];
-     console.log(this.newLocations);
+
 
     this.dataSource = new MatTableDataSource(this.newLocations);
 }
+
+ async search() {
+
+   this.locations = await this.locationApiService.get('');
+
+  }
+  async save() {
+    this.locations = await this.locationApiService.post('');
+    console.log(this.locations);
+  }
 }
 
-
-
-
-
- // constructor(private locationsApiService: LocationApiService) { }
-
-//   ngOnInit() {
-//   }
-// async search() {
-
-//  this.data = await this.locationsApiService.get(this.location);
-
-// }
-// async save() {
-//   this.data = await this.locationsApiService.post(this.location);
-//   console.log(this.data);
-// }
-// }
